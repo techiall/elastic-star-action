2438628497f8ab7c7e2e9d5ec224901190a31f94
techial <techial1042@gmail.com>
2022-07-11 10:48:30 +0800

    Polish

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 version=$(cat .env | sed 's/ES_VERSION=\(.*\)$/\1/g')
 
 docker-compose -f docker-compose-build.yml build
-docker tag elastic-star-action_elasticsearch:latest docker.pkg.github.com/techial1042/elastic-star-action/es:${version}
-docker push docker.pkg.github.com/techial1042/elastic-star-action/es:${version}
+docker tag elastic-star-action_elasticsearch:latest dghcr.io/techiall/elastic-star-action/es:${version}
+docker push ghcr.io/techiall/elastic-star-action/es:${version}
